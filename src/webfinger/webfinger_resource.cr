ec3040c33d7728e6f58f6393d83b9ad0d71afc6f
Jose Chavez <jose.e.chavez@gmail.com>
2023-12-13 22:34:08 -0800

    Minor change

--- src/webfinger/webfinger_resource.cr
+++ src/webfinger/webfinger_resource.cr
@@ -8,16 +8,20 @@ module Smilodon::Webfinger
       # TODO - handle the rel query parameter
       get "/.well-known/webfinger" do |env|
         resource = env.params.query["resource"]?
+
         if (resource.nil? || resource.empty?)
           halt env, status_code: 400, response: "Missing resource parameter"
         end
+
         account = @webfinger_handler.find_account(resource)
+
         if account.nil?
           halt env, status_code: 404, response: "Account Not Found"
         end
 
         env.response.headers["Access-Control-Allow-Origin"] = "*"
         env.response.content_type = "application/jrd+json"
+
         account.to_json
       end
     end
